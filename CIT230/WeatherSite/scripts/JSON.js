c4806d6dfea75d930ad1253c47bd4bfcced83e9e
Miladybug <emmarj24601@gmail.com>
2018-07-16 17:26:34 -0600

    Planning Document + some weather meddling

--- CIT230/WeatherSite/scripts/JSON.js
+++ CIT230/WeatherSite/scripts/JSON.js
@@ -8,22 +8,32 @@ request.send();
 
 request.onload = function () {
     var Voltron = request.response;
-    franklin(Voltron);
+    Shiro(Voltron);
 }
 
-function franklin(jsonObj) {
-    var towns = jsonObj['towns']
-    for (var i = 0; i < towns.length; i++) {
-        if (i == 2){
+function Shiro(jsonObj) {
+    var towns = jsonObj['towns'];
+
+    for (var i = 0; i < 4; i++) {
+        if (i == 2) {
             continue;
         }
-        var myArticle = document.createElement('article');
+
+        if (i == 0) {
+            var myArticle = document.getElementById('Home-Franklin');
+        } 
+        else if (i == 1) {
+            var myArticle = document.getElementById('Home-Greenville');
+        }
+        else if (i == 3) {
+            var myArticle = document.getElementById('Home-Springfield');
+        }
+
         var Kuro = document.createElement('h1');
         var motto = document.createElement('p');
         var founded = document.createElement('p');
         var population = document.createElement('p');
         var rainfall = document.createElement('p');
-        //var list = document.createElement('ul');
 
         Kuro.textContent = towns[i].name;
         motto.textContent = 'Motto: ' + towns[i].motto
@@ -31,20 +41,10 @@ function franklin(jsonObj) {
         population.textContent = 'Current Population: ' + towns[i].currentPopulation;
         rainfall.textContent = 'Average Rainfall: ' + towns[i].averageRainfall + '"';
 
-        //            var quelquechose = towns[i].events;
-        //                for (var j = 0; j < quelquechose.length; j++){
-        //                    var listItem = document.createElement('li');
-        //                    listItem.textContent = quelquechose[j];
-        //                    list.appendChild(listItem);
-        //                }
-
         myArticle.appendChild(Kuro);
         myArticle.appendChild(motto);
         myArticle.appendChild(founded);
         myArticle.appendChild(population);
         myArticle.appendChild(rainfall);
-        //myArticle.appendChild(list);
-
-        main.appendChild(myArticle);
     }
 }
