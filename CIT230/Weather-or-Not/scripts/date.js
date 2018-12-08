a4925af9673e659822da35b650c08d90b10c8a7f
Miladybug <emmarj24601@gmail.com>
2018-12-08 16:46:59 -0700

    ?

--- CIT230/Weather-or-Not/scripts/date.js
+++ CIT230/Weather-or-Not/scripts/date.js
@@ -15,6 +15,21 @@ function getDate() {
     document.getElementById("currentdate").innerHTML = dayOfWeek + ", " + d.getDate() + " " + monthFinal + " " + d.getFullYear();
 }
 
-function dropdown(){
+function dropdown() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+
+function windChill() {
+    var high = parseFloat(document.getElementById("high-temp").innerHTML);
+    var low = parseFloat(document.getElementById("low-temp").innerHTML);
+    var t = (high + low) / 2;
+    var s = parseFloat(document.getElementById("wind-speed").innerHTML);  
+    
+    var f = 35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * t * Math.pow(s, 0.16));
+    
+    var digits = 2;
+    var multiplier = Math.pow(10, digits);
+    f = Math.round(f * multiplier) / multiplier;
+    
+    document.getElementById("wind-temp").innerHTML = f;
 }
